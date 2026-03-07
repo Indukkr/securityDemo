1611dc138311d84598647c087b6fffcb05a86a5b
indu <patel.indu02@gmail.com>
2026-03-07 08:16:02 +0530

    Creating custom filter for JWT authentication with Spring Security !!

--- securityDemo/src/main/java/com/springsecurity/securityDemo/jwt/AuthTokenFilter.java
+++ securityDemo/src/main/java/com/springsecurity/securityDemo/jwt/AuthTokenFilter.java
@@ -49,7 +49,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             }
             
         } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}",e);
+            logger.error("Cannot set user authentication: { }",e);
         }
         filterChain.doFilter(request,response);
 
